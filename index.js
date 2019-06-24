3a8f315eaa5bf1f35f7697b1b7cc05c442dfe40b
keithburgie <krburgie@gmail.com>
2019-06-24 13:05:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
-  document.getElementById('text').textContent = "This is really cool!";
+  document.getElementById('text').innerHTML = "This is really cool!";
 });
 
 console.log("DOM not loaded yet!");
