c5dd801f33b719a8bb28ee6f7dce993bf0b488fc
Matt Williams <matt.seongjun@gmail.com>
2019-06-24 11:59:33 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    let text = document.getElementById('text');
+    text.textContent = "This is really cool!";
+});
