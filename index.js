edf525c3fcb465e044d8b944931f7f1ce46729d7
villatrue <scarter0313@gmail.com>
2019-06-24 17:29:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let here = document.getElementById("text")
+    here.innerHTML = "This is really cool!"
+  });
