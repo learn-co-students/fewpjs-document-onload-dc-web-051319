81cf0ae58ac47abeaad6add30e99cf695b0a34b0
Shujaat Azim <shujaat.azim@gmail.com>
2019-06-25 12:03:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let paragraph = document.getElementById("text");
+    paragraph.innerHTML = "This is really cool!"
+  });
+
